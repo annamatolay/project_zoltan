3da01291b2cbd258474f261b09bd8a68d0f601f6
Codecool-Palko <matolaypal@gmail.com>
2016-11-30 08:05:56 +0100

    skeleton of server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/1")
+def one():
+    return render_template("/chapters/1.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
