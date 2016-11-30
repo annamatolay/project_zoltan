668b10836f9c2c893a9e9ed4e76d9d2759d65ccb
Codecool-Palko <matolaypal@gmail.com>
2016-11-30 09:59:08 +0100

    implement all routes, what necessary

--- server.py
+++ server.py
@@ -9,9 +9,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/1")
-def one():
-    return render_template("/chapters/1.html")
+@app.route("/page/<page_number>")
+def page(page_number):
+    return render_template("/page.html", page=page_number)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
